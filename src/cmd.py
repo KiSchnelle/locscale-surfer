3c4eef33ee04b2e9ae96e4acf8e79b61928ba07c
Kilian Schnelle <kischnelle@icloud.com>
2025-12-19 10:59:06 +0100

    formatting

--- src/cmd.py
+++ src/cmd.py
@@ -6,8 +6,6 @@ the Author(s).
 Copyright (C) 2025 Arjen J. Jakobi (TU Delft), Alok Bhradwaj (TU Delft) and Lotte Verbeek (TU Delft)
 
 """
-from chimerax.core.commands import CmdDesc      # Command description
-import chimerax
 
 # from chimerax.atomic import AtomsArg            # Collection of atoms argument
 # from chimerax.core.commands import BoolArg      # Boolean argument
@@ -15,9 +13,12 @@ import chimerax
 # from chimerax.core.commands import IntArg       # Integer argument
 # from chimerax.core.commands import EmptyArg     # (see below)
 # from chimerax.core.commands import Or, Bounded  # Argument modifiers
-from chimerax.core.commands import ModelArg     # Model argument
-from chimerax.map_data import ArrayGridData
+from chimerax.core.commands import (
+    CmdDesc,  # Command description
+    ModelArg,  # Model argument
+)
 from chimerax.map import volume_from_grid_data
+from chimerax.map_data import ArrayGridData
 
 # from chimerax.core.commands import run
 
@@ -26,17 +27,22 @@ from chimerax.map import volume_from_grid_data
 # Functions and descriptions for registering using ChimeraX bundle API
 # ==========================================================================
 
+
 def segment_map(session, inputMap=None, inputMask=None):
-    ''' 
-    Function to segment an input map containing a detergent micelle 
-    '''
+    """
+    Function to segment an input map containing a detergent micelle
+    """
     assert inputMap is not None, "Please provide a map to segment"
-    import os 
-    # Import necessary packages 
+    import os
+
+    # Import necessary packages
     from .surfer import predict
-    #model_state_path = "U:\ajlab\AB\parking_for_files\fromDB\segmentation_micelle_with_curateD_micelle_outputdata\20250208_103426_segmentation_scunet_curated_cz48_BCEloss_without_dilation\saved_models\model_20250208_103426_8.pt"
+
+    # model_state_path = "U:\ajlab\AB\parking_for_files\fromDB\segmentation_micelle_with_curateD_micelle_outputdata\20250208_103426_segmentation_scunet_curated_cz48_BCEloss_without_dilation\saved_models\model_20250208_103426_8.pt"
     print("__file__", __file__)
-    model_state_path = os.path.join(os.path.dirname(__file__), "dataDir", "model_20250208_103426_8.pt")
+    model_state_path = os.path.join(
+        os.path.dirname(__file__), "dataDir", "model_20250208_103426_8.pt"
+    )
     print("model_state_path", model_state_path)
     # check if the model_state_path exists
     if not os.path.exists(model_state_path):
@@ -59,19 +65,26 @@ def segment_map(session, inputMap=None, inputMask=None):
         mask = None
 
     output_array = predict(
-                        emmap, apix[0], mask,\
-                        batch_size = 8, cube_size = 48, step_size = 32, gpu_ids = [0], model_state_path=model_state_path
-                    )
+        emmap,
+        apix[0],
+        mask,
+        batch_size=8,
+        cube_size=48,
+        step_size=32,
+        gpu_ids=[0],
+        model_state_path=model_state_path,
+    )
 
     # Create a new map
     new_map = ArrayGridData(output_array, origin=origin, step=apix)
 
     new_map.name = "Segmented map"
     print(f"New map name: {new_map.name}")
-    new_volume = volume_from_grid_data(new_map, session)
-        
-    
-surfer_desc = CmdDesc(optional=[("inputMap", ModelArg),("inputMask", ModelArg)])
+    volume_from_grid_data(new_map, session)
+
+
+surfer_desc = CmdDesc(optional=[("inputMap", ModelArg), ("inputMask", ModelArg)])
+
 
 def open_segmentation_tool(session):
     """
@@ -79,15 +92,18 @@ def open_segmentation_tool(session):
     for the map and mask and then run the segmentation.
     """
     from .tool import SegmentMapTool
+
     # Create an instance of the tool widget.
     tool_widget = SegmentMapTool(session)
     # Add the tool widget to the ChimeraX UI (here, in the side area).
     session.ui.tools.add_tool(tool_widget, area="side", floating=False)
 
+
 # Command descriptor for opening the tool.
 open_tool_desc = CmdDesc(optional=[])
 
+
 def register_tool_command(logger):
     from chimerax.core.commands import register
+
     register("open segment tool", open_segmentation_tool, open_tool_desc, logger=logger)
-                        
